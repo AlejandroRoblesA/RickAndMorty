fc38898dfe105cb79f1a95ffd31ff01f3407ae54
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-08-29 18:17:15 -0600

    Enum endpoint

---
+++ RickAndMorty/APIClient/Endpoint.swift
@@ -0,0 +1,14 @@
+//
+//  Endpoint.swift
+//  RickAndMorty
+//
+//  Created by Alejandro Robles on 29/08/23.
+//
+
+import Foundation
+
+@frozen enum Endpoint: String {
+    case character
+    case location
+    case episode
+}
