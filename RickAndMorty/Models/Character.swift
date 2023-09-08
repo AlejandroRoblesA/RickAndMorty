9053501f67833f9af9ce5306881a646f685e4229
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-09-07 19:38:27 -0600

    Character Model

--- RickAndMorty/Models/Character.swift
+++ RickAndMorty/Models/Character.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct Character: Codable {
+    let id: Int
+    let name: String
+    let status: String
+    let species: String
+    let type: String
+    let gender: String
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
