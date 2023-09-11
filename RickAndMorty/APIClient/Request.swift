7f68b8a6d7ddabbc69577fc97eb3135402a22e02
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-09-11 12:02:16 -0600

    Build API Request

--- RickAndMorty/APIClient/Request.swift
+++ RickAndMorty/APIClient/Request.swift
@@ -10,5 +10,44 @@ import Foundation
 
 /// Object that represents a single API call
 final class Request {
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+    private let endpoint: Endpoint
+    private let pathComponents: [String]
+    private let queryParameters: [URLQueryItem]
     
+    /// Constructed URL for the API Request in string format
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach {
+                string += "/\($0)"
+            }
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap {
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }.joined(separator: "&")
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    public init(endpoint: Endpoint, pathComponents: [String] = [], queryParameters: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
