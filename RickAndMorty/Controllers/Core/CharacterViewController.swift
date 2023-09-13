79db37bd0d8d6cf0e731300453e8d181824f15ae
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-09-13 16:45:13 -0600

    SubView CharacterListView Added On CharacterViewController

--- RickAndMorty/Controllers/Core/CharacterViewController.swift
+++ RickAndMorty/Controllers/Core/CharacterViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 /// Controller to show and search for Characters
 final class CharacterViewController: UIViewController {
 
+    private let characterListView = CharacterListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         title = "Characters"
         
+        view.addSubview(characterListView)
         
+        NSLayoutConstraint.activate([
+            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
