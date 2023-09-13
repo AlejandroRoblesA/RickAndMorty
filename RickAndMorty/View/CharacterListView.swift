165ad95967f3d92ae3a24ee5325fac1bc5705d3d
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-09-13 16:51:14 -0600

    Spinner created

--- RickAndMorty/View/CharacterListView.swift
+++ RickAndMorty/View/CharacterListView.swift
@@ -9,13 +9,34 @@ import UIKit
 
 /// View that habdles showing list of characters, loader, etc.
 final class CharacterListView: UIView {
+    
+    private let viewModel = CharacterListViewModel()
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = .systemBlue
+        addSubview(spinner)
+        addConstraints()
+        spinner.startAnimating()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
     }
 }
