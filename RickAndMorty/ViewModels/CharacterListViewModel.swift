8ec19f4d3cf7971f2d121ef368120f466cab3580
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-09-13 17:23:28 -0600

    CollectionView CharacterList

--- RickAndMorty/ViewModels/CharacterListViewModel.swift
+++ RickAndMorty/ViewModels/CharacterListViewModel.swift
@@ -5,9 +5,10 @@
 //  Created by Alejandro Robles on 13/09/23.
 //
 
-import Foundation
+import UIKit
 
-struct CharacterListViewModel {
+final class CharacterListViewModel: NSObject {
+   
     func fetchCharacters() {
         Service.shared.execute(.listCharactersRequest, expecting: GetAllCharactersResponse.self) { result in
             switch result {
@@ -17,5 +18,26 @@ struct CharacterListViewModel {
                 print(String(describing: error))
             }
         }
+    }
+}
+
+extension CharacterListViewModel: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+}
+
+extension CharacterListViewModel: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let bounds = UIScreen.main.bounds
+        let width = (bounds.width-30)/2
+        let height = width * 1.5
+        return CGSize(width: width, height: height)
     }
 }
