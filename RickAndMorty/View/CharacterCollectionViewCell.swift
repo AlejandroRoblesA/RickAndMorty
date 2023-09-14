fab635243e52f6645772cbaac65010a6da314a20
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-09-14 12:04:27 -0600

    CharacterCollectionViewCell ViewModel

--- RickAndMorty/View/CharacterCollectionViewCell.swift
+++ RickAndMorty/View/CharacterCollectionViewCell.swift
@@ -11,9 +11,35 @@ import UIKit
 final class CharacterCollectionViewCell: UICollectionViewCell {
     static let cellIdentifier = "CharacterCollectionViewCell"
     
+    private let imageView: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubviews(imageView, nameLabel, statusLabel)
+        addConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -21,11 +47,16 @@ final class CharacterCollectionViewCell: UICollectionViewCell {
     }
     
     private func addConstraints() {
-        
+        NSLayoutConstraint.activate([
+            
+        ])
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        imageView.image = nil
+        nameLabel.text = nil
+        statusLabel.text = nil
     }
     
     public func configure(with viewModel: CharacterCollectionViewCellViewModel) {
--- RickAndMorty/ViewModels/CharacterCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/CharacterCollectionViewCellViewModel.swift
@@ -7,6 +7,36 @@
 
 import Foundation
 
-struct CharacterCollectionViewCellViewModel {
+final class CharacterCollectionViewCellViewModel {
     
+    public let characterName: String
+    private let characterStatus: CharacterStatus
+    private let characterImageUrl: URL?
+    
+    public var characterStatusText: String {
+        return characterStatus.rawValue
+    }
+    
+    // MARK: - Init
+    init(characterName: String, characterStatus: CharacterStatus, characterImageUrl: URL?) {
+        self.characterName = characterName
+        self.characterStatus = characterStatus
+        self.characterImageUrl = characterImageUrl
+    }
+    
+    public func fectchImage(completion: @escaping (Result<Data, Error>) -> Void) {
+        // TODO: Abstract to Image Manager
+        guard let url = characterImageUrl else {
+            completion(.failure(URLError(.badURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            completion(.success(data))
+        }.resume()
+    }
 }
