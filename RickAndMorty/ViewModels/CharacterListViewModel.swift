0496a6ec41b47546ec473cee8c7b04e241d6cbf6
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-09-14 11:35:04 -0600

    CharacterCollectionViewCell Registered

--- RickAndMorty/ViewModels/CharacterListViewModel.swift
+++ RickAndMorty/ViewModels/CharacterListViewModel.swift
@@ -27,8 +27,7 @@ extension CharacterListViewModel: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .blue
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharacterCollectionViewCell.cellIdentifier, for: indexPath)
         return cell
     }
 }
