0a00c4ca64046de23bf9126e1ac44972bb4c6c12
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-09-21 16:57:15 -0600

    CharacterDetailViewController File

---
+++ RickAndMorty/Controllers/Other/CharacterDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterDetailViewController.swift
+//  RickAndMorty
+//
+//  Created by Alejandro Robles on 21/09/23.
+//
+
+import UIKit
+
+/// Controller to show info about single character
+class CharacterDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
