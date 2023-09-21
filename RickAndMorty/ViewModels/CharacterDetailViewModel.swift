ec095c24585f97289be30bec7f7c4d905fddf7ff
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-09-21 17:06:00 -0600

    CharacterDetailViewModel File

---
+++ RickAndMorty/ViewModels/CharacterDetailViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterDetailViewModel.swift
+//  RickAndMorty
+//
+//  Created by Alejandro Robles on 21/09/23.
+//
+
+import Foundation
+
+final class CharacterDetailViewModel {
+    init () {
+    }
+}
