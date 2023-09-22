288bab4947f582e2b977ca94e6c2061d980d395a
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-09-22 13:53:54 -0600

    FooterLoadingCollectionReusableView File

---
+++ RickAndMorty/View/FooterLoadingCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  FooterLoadingCollectionReusableView.swift
+//  RickAndMorty
+//
+//  Created by Alejandro Robles on 22/09/23.
+//
+
+import UIKit
+
+final class FooterLoadingCollectionReusableView: UICollectionReusableView {
+        
+}
