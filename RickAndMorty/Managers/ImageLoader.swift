f8450e58c746d8f8acc3471928a23c99e976c92c
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-09-27 17:19:06 -0600

    Download Image Function

--- RickAndMorty/Managers/ImageLoader.swift
+++ RickAndMorty/Managers/ImageLoader.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+final class ImageLoader {
+    static let shared = ImageLoader()
+    private init(){}
+    
+    func downloadImage(_ url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            completion(.success(data))
+        }.resume()
+    }
+}
