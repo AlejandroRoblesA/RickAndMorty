d5552ac2b09b2e38b468858eccd19d49b36fde1c
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-09-27 17:32:27 -0600

    Image Data Cache

--- RickAndMorty/Managers/ImageLoader.swift
+++ RickAndMorty/Managers/ImageLoader.swift
@@ -9,15 +9,31 @@ import Foundation
 
 final class ImageLoader {
     static let shared = ImageLoader()
+    private var imageDataCache = NSCache<NSString, NSData>()
     private init(){}
     
+    /// Get Image With URL
+    /// - Parameters:
+    ///   - url: Source URL
+    ///   - completion: Callback
     func downloadImage(_ url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        let key = url.absoluteString as NSString
+        if let data = imageDataCache.object(forKey: key){
+            completion(.success(data as Data))
+            return
+        }
         let request = URLRequest(url: url)
-        URLSession.shared.dataTask(with: request) { data, _, error in
-            guard let data = data, error == nil else {
+        URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            guard let data = data,
+                  error == nil,
+                  let self = self
+            else {
                 completion(.failure(error ?? URLError(.badServerResponse)))
                 return
             }
+            let value = data as NSData
+            let key = url.absoluteString as NSString
+            self.imageDataCache.setObject(value, forKey: key)
             completion(.success(data))
         }.resume()
     }
