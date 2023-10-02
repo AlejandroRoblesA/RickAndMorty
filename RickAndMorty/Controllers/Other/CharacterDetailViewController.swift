ba7f8d9b81d097f5dd5a699e7c83e8cb5c727bcf
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-10-02 12:16:56 -0600

    CharacterDetailView Added As SubView In CharacterDetailViewController

--- RickAndMorty/Controllers/Other/CharacterDetailViewController.swift
+++ RickAndMorty/Controllers/Other/CharacterDetailViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 /// Controller to show info about single character
 class CharacterDetailViewController: UIViewController {
     private var viewModel: CharacterDetailViewModel
+    private let detailView = CharacterDetailView()
     
+    // MARK: - Init
     init(viewModel: CharacterDetailViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -25,5 +27,16 @@ class CharacterDetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(detailView)
+        addConstraints()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            detailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
