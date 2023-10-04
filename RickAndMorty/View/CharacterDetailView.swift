59522f9626e968c64e1e0233059be2cee63ac6f6
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-10-04 13:38:51 -0600

    Info Section Layout

--- RickAndMorty/View/CharacterDetailView.swift
+++ RickAndMorty/View/CharacterDetailView.swift
@@ -88,15 +88,15 @@ final class CharacterDetailView: UIView {
     }
     
     private func createInfoSectionLayout() -> NSCollectionLayoutSection {
-        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5),
                                                                              heightDimension: .fractionalHeight(1.0)))
-        item.contentInsets = NSDirectionalEdgeInsets(top: 0,
-                                                     leading: 0,
-                                                     bottom: 10,
-                                                     trailing: 0)
-        let group = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+        item.contentInsets = NSDirectionalEdgeInsets(top: 2,
+                                                     leading: 2,
+                                                     bottom: 2,
+                                                     trailing: 2)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                                                                         heightDimension: .absolute(150)),
-                                                     subitems: [item])
+                                                     subitems: [item, item])
         let section = NSCollectionLayoutSection(group: group)
         return section
     }
