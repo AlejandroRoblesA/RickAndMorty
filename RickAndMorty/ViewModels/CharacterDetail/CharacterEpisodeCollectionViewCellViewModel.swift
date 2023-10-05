eda79aa28819376d79407bc83dd6331d077809c7
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-10-05 13:45:11 -0600

    CollectionViewCell ViewModels

---
+++ RickAndMorty/ViewModels/CharacterDetail/CharacterEpisodeCollectionViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterEpisodeCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Alejandro Robles on 05/10/23.
+//
+
+import Foundation
---
+++ RickAndMorty/ViewModels/CharacterDetail/CharacterPhotoCollectionViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterPhotoCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Alejandro Robles on 05/10/23.
+//
+
+import Foundation
