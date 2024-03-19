2fbff81f36d73b660a76b5973b5945dc64b9fd62
Alejandro Robles <alejandro_r1704@hotmail.com>
2024-03-19 15:39:54 -0600

    Fetch Data Just When Is Needed

--- RickAndMorty/ViewModels/CharacterDetail/CharacterEpisodeCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/CharacterDetail/CharacterEpisodeCollectionViewCellViewModel.swift
@@ -7,16 +7,40 @@
 
 import Foundation
 
+protocol EpisodeDataRenderProtocol {
+    var name: String { get }
+    var air_date: String { get }
+    var episode: String { get }
+}
+
 final class CharacterEpisodeCollectionViewCellViewModel {
     private let episodeDataUrl: URL?
     private var isFetching: Bool = false
+    private var dataBlock: ((EpisodeDataRenderProtocol) -> Void)?
+    private var episode: Episode? {
+        didSet {
+            guard let model = episode else { return }
+            self.dataBlock?(model)
+        }
+    }
     //MARK: - Init
     init(episodeDataUrl: URL?) {
         self.episodeDataUrl = episodeDataUrl
     }
     
+    public func registerForData(_ block: @escaping (EpisodeDataRenderProtocol) -> Void) {
+        self.dataBlock = block
+    }
+    
     public func fetchEpisode() {
-        guard !isFetching else { return }
+        guard
+            !isFetching
+        else {
+            if let model = episode {
+                dataBlock?(model)
+            }
+            return
+        }
         guard
             let url = episodeDataUrl,
             let request = Request(url: url)
@@ -24,10 +48,12 @@ final class CharacterEpisodeCollectionViewCellViewModel {
             return
         }
         isFetching = true
-        Service.shared.execute(request, expecting: Episode.self) { result in
+        Service.shared.execute(request, expecting: Episode.self) { [weak self] result in
             switch result {
-            case .success(let success):
-                print(String(describing: success.id))
+            case .success(let model):
+                DispatchQueue.main.async {
+                    self?.episode = model
+                }
             case .failure(let failure):
                 print(String(describing: failure))
             }
