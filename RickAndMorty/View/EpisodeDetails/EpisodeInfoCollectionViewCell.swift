f4abbcfc589b789b0f491ec2fe592fa925b783de
Alejandro Robles <alejandro_r1704@hotmail.com>
2024-05-07 11:54:50 -0600

    Episode Detail Layout

---
+++ RickAndMorty/View/EpisodeDetails/EpisodeInfoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  EpisodeInfoCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by Alejandro Robles on 07/05/24.
+//
+
+import UIKit
+
+final class EpisodeInfoCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ RickAndMorty/ViewModels/EpisodeDetail/EpisodeInfoCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EpisodeInfoCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Alejandro Robles on 07/05/24.
+//
+
+import Foundation
+
+struct EpisodeInfoCollectionViewCellViewModel {
+    public let title: String
+    public let value: String
+}
