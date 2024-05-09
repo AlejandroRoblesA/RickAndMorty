5cae6cec044fc4a086b85c83018021e96a531afb
Alejandro Robles <alejandro_r1704@hotmail.com>
2024-05-09 12:13:07 -0600

    Navigation Episode Details

--- RickAndMorty/View/EpisodeDetails/EpisodeInfoCollectionViewCell.swift
+++ RickAndMorty/View/EpisodeDetails/EpisodeInfoCollectionViewCell.swift
@@ -10,10 +10,28 @@ import UIKit
 final class EpisodeInfoCollectionViewCell: UICollectionViewCell {
     static let cellIdentifier = "EpisodeInfoCollectionViewCell"
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.textAlignment = .right
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubviews(titleLabel, valueLabel)
         setupLayer()
+        addConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -22,10 +40,13 @@ final class EpisodeInfoCollectionViewCell: UICollectionViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        titleLabel.text = nil
+        valueLabel.text = nil
     }
     
     func configure(with viewModel: EpisodeInfoCollectionViewCellViewModel) {
-        
+        titleLabel.text = viewModel.title
+        valueLabel.text = viewModel.value
     }
     
     private func setupLayer() {
@@ -33,5 +54,19 @@ final class EpisodeInfoCollectionViewCell: UICollectionViewCell {
         layer.masksToBounds = true
         layer.borderWidth = 1
         layer.borderColor = UIColor.secondaryLabel.cgColor
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4),
+            titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -4),
+            titleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.47),
+            
+            valueLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4),
+            valueLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10),
+            valueLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -4),
+            valueLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.47),
+        ])
     }
 }
